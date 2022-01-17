e7f938be2fe9ebc378b165cbbc7f05e294982780
yahya <errafiyyahya7@gmail.com>
2022-01-17 11:52:12 +0100

    make it better

--- src/main/java/com/example/demo/auth/LoginSuccessHandler.java
+++ src/main/java/com/example/demo/auth/LoginSuccessHandler.java
@@ -25,9 +25,9 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
        String redirectURL = request.getContextPath();
         
        if (userDetails.hasRole("admin")) {
-           redirectURL = "index.html";
+           redirectURL = "./index.html";
        } else if (userDetails.hasRole("user")) {
-           redirectURL = "indexuser.html";
+           redirectURL = "./indexuser.html";
        } 
        int m = (int) userDetails.getUserId(); 
        
